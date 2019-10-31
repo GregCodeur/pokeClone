2257a4d72851290b87b0e7a19e0bf5875e9d7370
GregCodeur <33575982+GregCodeur@users.noreply.github.com>
2019-10-31 15:00:45 +0100

    Create enum.lua

---
+++ enum.lua
@@ -0,0 +1,12 @@
+Enum = {}
+
+Enum.TypeTile = {};
+
+--TypeTile
+Enum.TypeTile.SolNeutre = "SolNeutre";
+Enum.TypeTile.SolHerbe = "SolHerbe";
+Enum.TypeTile.HautesHerbe = "HautesHerbe";
+
+--Fin TypeTile
+
+return Enum;
