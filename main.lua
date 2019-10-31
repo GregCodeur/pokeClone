14d33f06e899dbc5aa497f0bf7b6aa949de048f8
GregCodeur <33575982+GregCodeur@users.noreply.github.com>
2019-10-31 14:59:44 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,24 @@
+-- Cette ligne permet d'afficher des traces dans la console pendant l'éxécution
+io.stdout:setvbuf('no')
+
+-- Empèche Love de filtrer les contours des images quand elles sont redimentionnées
+-- Indispensable pour du pixel art
+love.graphics.setDefaultFilter("nearest")
+
+-- Cette ligne permet de déboguer pas à pas dans ZeroBraneStudio
+if arg[#arg] == "-debug" then require("mobdebug").start() end
+
+local myGame = require("game");
+
+function love.load()
+  myGame.Load();
+  
+end
+
+function love.update(dt)
+  myGame.Update(dt)
+end
+
+function love.draw()
+  myGame.Draw();
+end
